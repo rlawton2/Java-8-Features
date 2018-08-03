2b8ae951f767f8f9ed939ce0bcf86aeb783c81ad
rlawton2 <rlawton1234@gmail.com>
2018-08-02 21:00:40 -0700

    functionality added to some classes

--- src/ConstructorReference.java
+++ src/ConstructorReference.java
@@ -1,9 +1,29 @@
-
+/*
+ * A simple demonstration of how to create a constructor reference
+ */
 public class ConstructorReference {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		// Method reference to constructor
+		Display dis = Hello::new;
+		dis.display("Hello from Arizona");
 
 	}
 
+}
+
+// Functional interface
+@FunctionalInterface
+interface Display {
+	
+	Hello display(String arg);
+}
+
+// Class with constructor
+class Hello {
+	
+	public Hello(String say) {
+		System.out.println(say);
+	}
 }
--- src/StaticReference.java
+++ src/StaticReference.java
@@ -1,9 +1,22 @@
-
+import java.util.function.BiFunction;
+/*
+ * The java library interface BiFunction<T,U,R> is used here.
+ *   T = first arg  U = second arg  R = result
+ * The method apply(T t, U u) belongs to the BiFuction interface
+ */
 public class StaticReference {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// Can call Addition method addition directly because it is static
+		BiFunction<Integer, Integer, Integer> sum = Calulate::addition;
+		System.out.println("The sum of 5 + 4 is " + sum.apply(5, 4));
 	}
+}
 
+// Class with a static method named addition
+class Calulate {
+	
+	public static int addition(int a, int b) {
+		return (a + b);
+	}
 }
